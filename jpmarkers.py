eb7a02eb775c366496c499c30009e4475b3528d7
Mattias Iko Mattsson <vitplister@gmail.com>
2023-09-23 23:34:29 +0200

    Now actually kind of works

--- jpmarkers.py
+++ jpmarkers.py
@@ -2,8 +2,25 @@
 
 import argparse
 import struct
+import os
 
 jpeg_markers = {
+    # 00, 01, FE, C0, DF
+    # Rec. ITU-T T.81 | ISO/IEC 10918-1
+
+    # F0-F6
+    # Rec. ITU-T T.84 | ISO/IEC 10918-3
+
+    # F7-F8
+    # Rec. ITU-T T.87 | ISO/IEC 14495-1
+
+    # 4F-6F, 90-93
+    # Rec. ITU-T T.800 | ISO/IEC 15444-1
+
+    # 30-3F
+    # Reserverd for definition as markers only (no marker segments)
+
+
     # 01-0F
                      0xFF01: "TEM",
                                       0xFF0A: "JXL",
@@ -65,30 +82,113 @@ jpeg_markers = {
 paramterless_jpeg_markers = [
     0xFF01, 0xFF0A,                 # TEM, JXL
 
+                            0xFF4F, # SOC
+
     0xFF30, 0xFF31, 0xFF32, 0xFF33, # RES (Reserved), guaranteed parameter less
     0xFF34, 0xFF35, 0xFF36, 0xFF37,
     0xFF38, 0xFF39, 0xFF3A, 0xFF3B,
     0xFF3C, 0xFF3D, 0xFF3E, 0xFF3F,
+
+                    0xFF92, 0xFF93, # EPH, SOD
 
     0xFFD0, 0xFFD1, 0xFFD2, 0xFFD3, # RST0, RST1, RST2, RST3
     0xFFD4, 0xFFD5, 0xFFD6, 0xFFD7, # RST4, RST5, RST6, RST7
     0xFFD8, 0xFFD9,                 # SOI, EOI/EOC
 ]
 
+valid_bitstream_markers = [
+    0xFF00,
+    0xFF90,                         # SOT
+    0xFFD0, 0xFFD1, 0xFFD2, 0xFFD3, # RST0, RST1, RST2, RST3
+    0xFFD4, 0xFFD5, 0xFFD6, 0xFFD7, # RST4, RST5, RST6, RST7
+    0xFFD8, 0xFFD9,                 # SOI, EOI/EOC
+]
+
+start_of_bitsream_markers = [
+    0xFF93, 0xFFDA,                 # SOD, SOS
+]
+
+end_of_bitstream_markers = [
+    0xFFDC, 0xFFD9, 0xFFDA, 0xFF90  # DNL, EOI/EOC, SOS, SOT
+]
+
+
+def read_image_data(f):
+
+    CHUNK_SIZE = 4096
+    data = b""
+    orig_fpos = f.tell()
+    offset = 0
+
+    while True:
+        chunk = f.read(CHUNK_SIZE)
+        if not chunk:
+            break  # End of file
+
+        data += chunk
+
+        index = 0
+
+        # spin through allowed markers in image data
+        while index != -1:
+            index = data.find(b'\xFF', offset)
+
+            if index == -1:
+                continue
+            # marker byte is last byte of current chunk
+            elif (index == len(data) - 1):
+                break
+
+            offset = index + 1
+
+            # get marker code and convert to int
+            marker_code = data[index + 1]
+
+            # 0xFF is used for padding
+            if marker_code == 0xFF:
+                offset = index + 1
+                continue
+
+            marker_code = marker_code | 0xFF00
+
+            if marker_code in end_of_bitstream_markers:
+                data = data[:index]
+                f.seek(orig_fpos + index) # seek back to end of data marker
+                return(data)
+
+        # if marker byte is last byte in chunk, seek back one byte and read another chunk
+        if (index == len(data) - 1):
+            if len(chunk) == CHUNK_SIZE:
+                offset = index
+                data = data[:-1]
+                f.seek(-1, os.SEEK_CUR)
+                continue
+            else:
+                return(data)  # End of file
+
 
 def read_jpeg_markers(file_path):
     try:
         with open(file_path, "rb") as f:
+            offset = 0  # Initialize the offset to 0
+            marker_code = 0
             while True:
+                if marker_code in start_of_bitsream_markers:
+                    data = read_image_data(f)
+                    print(f"{offset:06X}: [bitstream data]")
+                    offset += len(data)
+
                 # Read the byte
                 marker_byte = f.read(1)
                 if not marker_byte:
                     break  # End of file
 
-                marker_byte = struct.unpack("B", marker_byte)[0]
+                # convert to int
+                marker_byte = ord(marker_byte)
 
                 # Check for valid marker (start with 0xFF)
                 if marker_byte != 0xFF:
+                    offset += 1
                     continue
 
                 # Get the marker code (second byte)
@@ -96,34 +196,41 @@ def read_jpeg_markers(file_path):
                 if not marker_code:
                     break  # End of file
 
-                marker_code = struct.unpack("B", marker_code)[0]
+                # convert to int
+                marker_code = ord(marker_code)
 
-                # Encoded 0xFF
-                if marker_code == 0x00:
+                # 0xFF is used for padding
+                if marker_code == 0xFF:
+                    offset += 1
                     continue
 
-                # Determine the marker name based on the code
-                marker_hexstring = f"0xFF{marker_code:02X}"
+                marker_code = marker_code | 0xFF00
 
-                # Only read segment length for marker that has it
-                if marker_code < 0xD0 or marker_code > 0xD9:
+                # Determine the marker name based on the code
+                marker_hexstring = f"{offset:06X}: 0x{marker_code:02X}"
+
+                # Only read segment length for markers with parameters
+                if marker_code not in paramterless_jpeg_markers:
                     # Read the marker segment length (two bytes)
                     segment_length_bytes = f.read(2)
                     segment_length = struct.unpack(">H", segment_length_bytes)[0]
 
                     # Read the marker segment data
-                    data_segment = f.read(
-                        segment_length - 2
-                    )  # Subtract 2 for the length bytes
+                    data_segment = f.read(segment_length - 2)
+                    if not data_segment:
+                        break   # End of file
 
-                # Convert marker bytes to an integer using struct.unpack
-                marker_int = marker_code | 0xFF00
+                    data_segment = segment_length_bytes + data_segment
+                    offset += len(data_segment)
 
-                marker_name = jpeg_markers.get(marker_int, "UNKNOWN")
+                # increase offset for marker bytes
+                offset += 2
 
-                # Print marker name and hex bytes
+                marker_name = jpeg_markers.get(marker_code, "UNKNOWN")
+
                 print(f"{marker_hexstring} ({marker_name})")
-    #                print(f"{marker_hexstring}")
+
+
     except FileNotFoundError:
         print("File not found.")
     except Exception as e:
